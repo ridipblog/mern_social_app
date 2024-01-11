569ecc986cbeb3c45e297ce002add184483a654e
unknown <memorytemp5@gmail.com>
2024-01-11 22:02:28 +0530

    Working Style In Chat Side Bar

--- login_front/src/components/chatsRooms/ChatRoom.js
+++ login_front/src/components/chatsRooms/ChatRoom.js
@@ -1,39 +1,44 @@
-import React, { useEffect, useState } from "react";
-import io from 'socket.io-client';
+// import React, { useEffect, useState } from "react";
+// import io from 'socket.io-client';
+import ChatSideBar from "./ChatSideBar";
+import "../class.css";
+// Import Main Css File For This File
 import "./chatroom.css";
+// Import Chat Side Bar
+import "./ChatSideBar.css";
 
 export default function ChatRoom() {
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-    const [personName, setPersonName] = useState('');
-    const [roomName, setRoomName] = useState('');
-    const socket = io('http://localhost:4000', { transports: ['websocket'] });
-    useEffect(() => {
-        socket.emit('join-room', roomName);
-        socket.on('message', (room, data) => {
-            const text = data.message + data.username;
-            setMessages((prev_messages) => [...prev_messages, text]);
-        });
-        return () => {
-            // socket.disconnect();
-            socket.emit('leave-room', roomName);
-        };
-    }, [roomName, messages, message]);
-    const sendMessage = () => {
-        const data = {
-            message: message,
-            username: 'coder'
-        };
-        const room = roomName;
-        socket.emit('message', { room, data });
-    }
-    // Set Person Name
-    const addPersonName = async () => {
-        setRoomName(personName);
-    }
+    // const [message, setMessage] = useState('');
+    // const [messages, setMessages] = useState([]);
+    // const [personName, setPersonName] = useState('');
+    // const [roomName, setRoomName] = useState('');
+    // const socket = io('http://localhost:4000', { transports: ['websocket'] });
+    // useEffect(() => {
+    //     socket.emit('join-room', roomName);
+    //     socket.on('message', (room, data) => {
+    //         const text = data.message + data.username;
+    //         setMessages((prev_messages) => [...prev_messages, text]);
+    //     });
+    //     return () => {
+    //         // socket.disconnect();
+    //         socket.emit('leave-room', roomName);
+    //     };
+    // }, [roomName, messages, message]);
+    // const sendMessage = () => {
+    //     const data = {
+    //         message: message,
+    //         username: 'coder'
+    //     };
+    //     const room = roomName;
+    //     socket.emit('message', { room, data });
+    // }
+    // // Set Person Name
+    // const addPersonName = async () => {
+    //     setRoomName(personName);
+    // }
     return (
         <>
-            <div className="">
+            {/* <div className="">
                 <div>
                     {
                         messages.map((msg, index) => (
@@ -46,6 +51,10 @@ export default function ChatRoom() {
                     <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
                     <button onClick={sendMessage}>Send</button>
                 </div>
+            </div> */}
+            <div className="flexDiv main-chat-div">
+                {/* Chat Side Bar Component */}
+                <ChatSideBar />
             </div>
         </>
     );
---
+++ login_front/src/components/chatsRooms/ChatSideBar.js
@@ -0,0 +1,15 @@
+export default function ChatSideBar() {
+    return (
+        <div className="flexDiv main-chat-side-div">
+            <div className="flexDiv chat-side-menu">
+                <p><i class="fa-brands fa-rocketchat"></i></p>
+            </div>
+            <div className="flexDiv chat-side-menu">
+                <p><i className="fa-solid fa-user-group"></i></p>
+                <p><i className="fa-solid fa-plus"></i></p>
+                <p><i className="fa-solid fa-gear"></i></p>
+                <p><i className="fa-solid fa-power-off"></i></p>
+            </div>
+        </div>
+    )
+}
